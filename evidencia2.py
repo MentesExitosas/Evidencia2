e6f21bb9ce3de154192869df22d6a5fc084324c4
PatricioBrenner <patricio.brenneres@uanl.edu.mx>
2020-11-09 18:56:21 -0600

    Add op 1

--- evidencia2.py
+++ evidencia2.py
@@ -1,11 +1,17 @@
 import numpy as np
 import pandas as pd
 
-
+diccionario_ventas = {}
 
 print("1. Registrar una venta\n 2. Consultar ventas de un día específico\n 3. Salir")
 menu = int(input("¿Qué opción desea elegir?"))
 
 try:
     if menu == 1:
-        
+        print("***REGISTRO DE VENTA***")
+        cantidad = int(input("¿Cuántos artículos deseas registrar en la venta?"))
+        for articulo in range (cantidad):
+            descripcion = input("Escriba la descripción del artículo:\n")
+            cant_piezas = int(input("¿Cuántas piezas vendió de este artículo?"))
+            precio_venta = int(input("¿Cuál fue el precio de venta del artículo"))
+    
