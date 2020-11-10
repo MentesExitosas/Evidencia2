c97789b20b5ac87e064aa1980bb403c8923e48b7
PatricioBrenner <patricio.brenneres@uanl.edu.mx>
2020-11-10 17:05:27 -0600

    Fix DataFrame

--- evidencia2.py
+++ evidencia2.py
@@ -36,10 +36,8 @@ while menu >= 1 and menu < 3:
         if menu == 2:
             pedirfecha = int(input("¿Cuál es la fecha que quieres consultar? "))
             print("Fecha procesada")
-            fecha_procesada = datetime.datetime.strptime(pedirfecha, "%d/%m/%y").date()
-            for elemento in ventasdf:
-                if fecha_procesada in ventasdf:
-                    print(elemento)
+
+            
     except Exception:   
         print(f"Ocurrió un error {sys.exc_info()[0]}")
 
