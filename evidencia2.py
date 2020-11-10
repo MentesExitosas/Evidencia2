e30a9dceab513c3211c4bc9b8ef1e7c89fe8f424
PatricioBrenner <patricio.brenneres@uanl.edu.mx>
2020-11-09 18:49:15 -0600

    Add libraries

--- evidencia2.py
+++ evidencia2.py
@@ -1,3 +1,11 @@
+import numpy as np
+import pandas as pd
+
+
+
 print("1. Registrar una venta\n 2. Consultar ventas de un día específico\n 3. Salir")
 menu = int(input("¿Qué opción desea elegir?"))
 
+try:
+    if menu == 1:
+        
