eac7a51f37981456b816056183c7891412902f05
PatricioBrenner <patricio.brenneres@uanl.edu.mx>
2020-11-09 19:03:16 -0600

    Add try

--- evidencia2.py
+++ evidencia2.py
@@ -1,17 +1,27 @@
 import numpy as np
 import pandas as pd
+import os
+import sys
+import time
 
 diccionario_ventas = {}
+menu = 1
 
-print("1. Registrar una venta\n 2. Consultar ventas de un día específico\n 3. Salir")
-menu = int(input("¿Qué opción desea elegir?"))
+while menu >= 1 and menu < 3:
+    print("\n1. Registrar una venta\n2. Consultar ventas de un día específico\n3. Salir")
+    menu = int(input("\n¿Qué opción desea elegir?\nOpción: "))
 
-try:
-    if menu == 1:
-        print("***REGISTRO DE VENTA***")
-        cantidad = int(input("¿Cuántos artículos deseas registrar en la venta?"))
-        for articulo in range (cantidad):
-            descripcion = input("Escriba la descripción del artículo:\n")
-            cant_piezas = int(input("¿Cuántas piezas vendió de este artículo?"))
-            precio_venta = int(input("¿Cuál fue el precio de venta del artículo"))
-    
+    try:
+        if menu == 1:
+            print("***REGISTRO DE VENTA***")
+            cantidad = int(input("¿Cuántos artículos deseas registrar en la venta?"))
+            for articulo in range (cantidad):
+                descripcion = input("Escriba la descripción del artículo:\n")
+                cant_piezas = int(input("¿Cuántas piezas vendió de este artículo?"))
+                precio_venta = int(input("¿Cuál fue el precio de venta del artículo"))
+            print("LISTO")
+
+    except Exception:   
+        print(f"Ocurrió un error {sys.exc_info()[0]}")
+
+
